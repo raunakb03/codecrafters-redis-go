e5c13d5e5d80df90349e488d0e1fe19844559fcf
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-11-03 20:18:17 +0530

    stage 3 responding to multiple pings

--- app/server.go
+++ app/server.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 var _ = net.Listen
 var _ = os.Exit
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.TrimSpace(text) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -15,15 +29,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-    for{
-        conn, err := l.Accept()
-        if err != nil {
-            fmt.Println("Error accepting connection: ", err.Error())
-            os.Exit(1)
-        }
-        go func(conn net.Conn){
-            defer conn.Close()
-            conn.Write([]byte("+PONG\r\n"))
-        }(conn)
-    }
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
 }
