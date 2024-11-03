60705c9bd1111943466afef45eca95904f42d0b1
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-11-03 23:27:48 +0530

    stage 5 implementing the echo command

--- app/server.go
+++ app/server.go
@@ -1,39 +1,68 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 var _ = net.Listen
 var _ = os.Exit
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	scanner := bufio.NewScanner(conn)
+func handleError(msg string, err error) {
+    log.Fatalf("%s : %s", msg, err)
+}
 
-	for scanner.Scan() {
-		text := scanner.Text()
-		if strings.TrimSpace(text) == "PING" {
-			conn.Write([]byte("+PONG\r\n"))
+func Ping(conn net.Conn) {
+	conn.Write([]byte("+PONG\r\n"))
+}
+
+func parseRequest(conn io.Reader) ([]string, error) {
+	msg := make([]byte, 1024)
+	value := []string{}
+	msglen, err := conn.Read(msg)
+	if err != nil {
+        handleError("Error reading from connection: ", err)
+	}
+	message := strings.TrimSpace(string(msg[:msglen]))
+	messageSlice := strings.Split(message, "\r\n")
+	for i, item := range messageSlice[2:] {
+		if i%2 == 0 {
+			value = append(value, item)
 		}
 	}
+	return value, nil
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+    decodedRequest, _ := parseRequest(conn)
+    res := "*"
+    res+= strconv.Itoa(len(decodedRequest))
+    res+="\r\n";
+    for _, item := range decodedRequest {
+        res+= "$"
+        res+= strconv.Itoa(len(item))
+        res+= "\r\n"
+        res+= item
+        res+= "\r\n"
+    }
+	conn.Write([]byte(res))
 }
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+        handleError("Error listening to port ", err)
 	}
+    defer l.Close()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+            handleError("Error accepting connection: ", err)
 		}
 		go handleConnection(conn)
 	}
